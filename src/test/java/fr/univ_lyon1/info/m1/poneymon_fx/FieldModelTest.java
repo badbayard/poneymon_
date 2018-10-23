958b4090de54145a8f06e0dbd5b04fc8a2252964
AliZahidi <a.alizahidi@gmail.com>
2018-10-24 00:53:59 +0200

    Bug fix tests

--- src/test/java/fr/univ_lyon1/info/m1/poneymon_fx/FieldModelTest.java
+++ src/test/java/fr/univ_lyon1/info/m1/poneymon_fx/FieldModelTest.java
@@ -3,21 +3,18 @@ package fr.univ_lyon1.info.m1.poneymon_fx;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-import fr.univ_lyon1.info.m1.poneymon_fx.model.PoneyModel;
-import fr.univ_lyon1.info.m1.poneymon_fx.model.FieldModel;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import fr.univ_lyon1.info.m1.poneymon_fx.model.FieldModel;
+import fr.univ_lyon1.info.m1.poneymon_fx.model.MovingEntityModel;
 
 /**
  * Unit test for the FieldModel class.
  */
- public class FieldModelTest {
-
-
+public class FieldModelTest {
 
     private FieldModel field;
-
 
     /**
      * initialisation du terrain
@@ -26,7 +23,6 @@ import org.junit.Test;
     public void setUp() {
         field = new FieldModel(5);
     }
-
 
     /**
      * Compare the number of poneys with the expected value
@@ -39,40 +35,35 @@ import org.junit.Test;
         FieldModel f3 = new FieldModel(4);
         FieldModel f4 = new FieldModel(12);
 
-        //When
+        // When
 
         // Then
-        assertEquals(f1.countPoneys(), 5);
-        assertEquals(f2.countPoneys(), 2);
-        assertEquals(f3.countPoneys(), 4);
-        assertEquals(f4.countPoneys(), 5);
+        assertEquals(f1.countParticipants(), 5);
+        assertEquals(f2.countParticipants(), 2);
+        assertEquals(f3.countParticipants(), 4);
+        assertEquals(f4.countParticipants(), 5);
     }
-
 
     /**
      * test de update() sur tous les poneys du terrain
      */
     @Test
-    public void StepWorksOnEveryPonys()
-    {
-        PoneyModel[] poneys = field.getPoneyModels();
+    public void StepWorksOnEveryPonys() {
+        MovingEntityModel[] poneys = field.getParticipantModels();
         double progression;
-        for(int i =0 ; i< poneys.length ; i++)
-        {
+        for (int i = 0; i < poneys.length; i++) {
             progression = poneys[i].totalProgress();
             poneys[i].update(500);
-            assert(poneys[i].totalProgress() >= progression);
+            assert (poneys[i].totalProgress() >= progression);
         }
     }
-
 
     /**
      * test de partieTerminee() si un poney atteind 5 tours
      */
     @Test
-    public void ReturnTrueIfGameIsOver()
-    {
-        PoneyModel[] poneys = field.getPoneyModels();
+    public void ReturnTrueIfGameIsOver() {
+        MovingEntityModel[] poneys = field.getParticipantModels();
 
         poneys[0].setX(0.99999999999);
         poneys[0].setNbLap(4);
@@ -91,30 +82,25 @@ import org.junit.Test;
         poneys[4].setX(0.99999999999);
         poneys[4].setNbLap(3);
 
-        for(int i = 0 ; i < poneys.length ; i++)
-        {
-            assertEquals(poneys[i].hasFinishedTheRace() , false);
+        for (int i = 0; i < poneys.length; i++) {
+            assertEquals(poneys[i].hasFinishedTheRace(), false);
         }
 
         field.update(500);
-        assertEquals( poneys[0].hasFinishedTheRace() , true);
-        assertEquals( poneys[1].hasFinishedTheRace() , true);
-        assertEquals( poneys[2].hasFinishedTheRace() , false);
-        assertEquals( poneys[3].hasFinishedTheRace() , false);
-        assertEquals( poneys[4].hasFinishedTheRace() , false);
+        assertEquals(poneys[0].hasFinishedTheRace(), true);
+        assertEquals(poneys[1].hasFinishedTheRace(), true);
+        assertEquals(poneys[2].hasFinishedTheRace(), false);
+        assertEquals(poneys[3].hasFinishedTheRace(), false);
+        assertEquals(poneys[4].hasFinishedTheRace(), false);
 
     }
-
 
     /**
      * la fonction de classement trie bien par progression totale
      */
     @Test
-    public void FieldOrderByTotalProgressionForRankingView()
-    {
-        PoneyModel[] poneys = field.getPoneyModels();
-
-
+    public void FieldOrderByTotalProgressionForRankingView() {
+        MovingEntityModel[] poneys = field.getParticipantModels();
 
         poneys[0].setX(0.99999999999);
         poneys[0].setNbLap(4);
@@ -133,20 +119,16 @@ import org.junit.Test;
 
         field.update(0);
 
-
         System.out.println("Classement voisin : 2 poneys equals ");
-        for(int i = 0 ; i < poneys.length ; i++)
-        {
-          System.out.println("Poney ["+i+"] rank : "+ poneys[i].getRank());
+        for (int i = 0; i < poneys.length; i++) {
+            System.out.println("Poney [" + i + "] rank : " + poneys[i].getRank());
         }
 
         System.out.println("\n");
 
-        int ArrayAttendu [] = {2,0,1,4,3};
-        field.rankPoney();
-        assertArrayEquals(field.getRanking(),ArrayAttendu);
-
-
+        int ArrayAttendu[] = { 2, 0, 1, 4, 3 };
+        field.rankParticipant();
+        assertArrayEquals(field.getRanking(), ArrayAttendu);
 
     }
 
@@ -154,12 +136,10 @@ import org.junit.Test;
      * la fonction de classement n'echange pas de place en cas d'egalité partielle dans le tableau
      */
     @Test
-    public void TestThreePoneyEquals()
-    {
-        PoneyModel[] poneys = field.getPoneyModels();
+    public void TestThreePoneyEquals() {
+        MovingEntityModel[] poneys = field.getParticipantModels();
 
-        for(int i = 0 ; i < poneys.length-2; i++)
-        {
+        for (int i = 0; i < poneys.length - 2; i++) {
             poneys[i].setX(0.99999999999);
             poneys[i].setNbLap(4);
             poneys[i].setSpeed(0.5);
@@ -177,34 +157,26 @@ import org.junit.Test;
         field.update(0);
 
         System.out.println("Classement voisin : 3 poneys equals ");
-        for(int i = 0 ; i < poneys.length ; i++)
-        {
-            System.out.println("Poney ["+i+"] rank : "+ poneys[i].getRank());
+        for (int i = 0; i < poneys.length; i++) {
+            System.out.println("Poney [" + i + "] rank : " + poneys[i].getRank());
         }
         System.out.println("\n");
 
-
-        int ArrayAttendu [] = {0,1,2,4,3};
-        field.rankPoney();
-        assertArrayEquals(field.getRanking(),ArrayAttendu);
-
-
+        int ArrayAttendu[] = { 0, 1, 2, 4, 3 };
+        field.rankParticipant();
+        assertArrayEquals(field.getRanking(), ArrayAttendu);
 
     }
-
-
 
     /**
      * la fonction de classement n'echange pas de place en cas d'egalité
      */
     @Test
-    public void FivePoneyEquals()
-    {
-        PoneyModel[] poneys = field.getPoneyModels();
-        int ArrayAttendu [] = new int [poneys.length];
+    public void FivePoneyEquals() {
+        MovingEntityModel[] poneys = field.getParticipantModels();
+        int ArrayAttendu[] = new int[poneys.length];
 
-        for(int i = 0 ; i < poneys.length; i++)
-        {
+        for (int i = 0; i < poneys.length; i++) {
             poneys[i].setX(0.99999999999);
             poneys[i].setNbLap(4);
             poneys[i].setSpeed(0.5);
@@ -214,29 +186,21 @@ import org.junit.Test;
         field.update(0);
 
         System.out.println("Classement voisin : 5 poneys equals ");
-        for(int i = 0 ; i < poneys.length ; i++)
-        {
-            System.out.println("Poney ["+i+"] rank : "+ poneys[i].getRank());
+        for (int i = 0; i < poneys.length; i++) {
+            System.out.println("Poney [" + i + "] rank : " + poneys[i].getRank());
         }
         System.out.println("\n");
 
-
-        field.rankPoney();
-        assertArrayEquals(field.getRanking(),ArrayAttendu);
+        field.rankParticipant();
+        assertArrayEquals(field.getRanking(), ArrayAttendu);
 
         System.out.println("Classement Tri : Verif ravail sur Copies ");
-        for(int i = 0 ; i < poneys.length ; i++)
-        {
-            System.out.println("Poney ["+i+"] Progression : "+ poneys[i].getX());
-            System.out.println("Poney ["+i+"] Tours : "+ poneys[i].getNbLap());
+        for (int i = 0; i < poneys.length; i++) {
+            System.out.println("Poney [" + i + "] Progression : " + poneys[i].getX());
+            System.out.println("Poney [" + i + "] Tours : " + poneys[i].getNbLap());
         }
         System.out.println("\n");
 
     }
-
-
-
-
-
 
 }
