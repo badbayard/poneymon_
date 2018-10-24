a8597cf41d4c436a0c07be2a8daf193918601e32
p1508302 <geoffroy.calon@etu.univ-lyon1.fr>
2018-10-24 15:06:12 +0200

    Working on the creation of the buttons according to the files available

--- src/main/java/fr/univ_lyon1/info/m1/poneymon_fx/view/SelectPoney.java
+++ src/main/java/fr/univ_lyon1/info/m1/poneymon_fx/view/SelectPoney.java
@@ -1,18 +1,11 @@
 package fr.univ_lyon1.info.m1.poneymon_fx.view;
 
-import java.io.IOException;
-import javafx.scene.Node;
+import fr.univ_lyon1.info.m1.poneymon_fx.model.AssetsRepertories;
 import javafx.scene.Parent;
-import javafx.scene.control.ChoiceBox;
-
-import javafx.scene.shape.Rectangle;
-
 import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.ImageView;
-
-import fr.univ_lyon1.info.m1.poneymon_fx.model.AssetsRepertories;
 
 /**
  * Class allowing the player to select a poney considering different entitys.  
@@ -21,7 +14,11 @@ public class SelectPoney extends Parent {
     
     private VBox poneyBox;
     private AssetsRepertories assetsRepertories;
-    //private String [] 
+    private String path;
+    private String [] availableEntity;
+    private String [] entityColor;
+    
+    private VBox [] entityBox;
     
     /**
      * Constructor.
@@ -30,13 +27,47 @@ public class SelectPoney extends Parent {
      */
     public SelectPoney(int x, int y) {
         
+        path = System.getProperty("user.dir") + "/src/main/resources/assets";
         
-        poneyBox = new VBox(10);
+        assetsRepertories  = new AssetsRepertories(path);
         
-        poneyBox.setTranslateX(x / 5);
-        poneyBox.setTranslateY(y / 5);
+        //entityName = assetsRepertories.filterNameEntity();
+        availableEntity = assetsRepertories.availableEntities();
         
-        Image image = new Image("assets/pony-blue.gif");
+        //entityColor = new String [availableEntity.length];
+        
+        entityBox = new VBox[availableEntity.length];
+        
+        for (int i = 0; i < availableEntity.length - 1; ++i) {
+            entityBox[i] = new VBox(10);
+            
+            entityBox[i].setTranslateX(x / (i+1));
+            entityBox[i].setTranslateY(y/ (i+1));
+            
+            entityColor = assetsRepertories.searchAndFilter(availableEntity[i] + "-[a-zA-Z]*(.gif)");
+            System.out.println(availableEntity[i]);
+            
+            for (int j = 0; j < entityColor.length; ++j) {
+                Image entityImage = new Image("assets/" + availableEntity[i] + "-" + entityColor[j] + ".gif");
+                ImageView imageView = new ImageView(entityImage);
+                imageView.setFitWidth(75);
+                imageView.setFitHeight(75);
+                
+                ToggleButton newButton = new ToggleButton("", imageView);
+                
+                System.out.println("J : " + j + " ," + entityColor[j]);
+                
+                entityBox[i].getChildren().add(newButton);
+            }
+            //getChildren().add(entityBox[i]);
+        }
+        
+        //poneyBox = new VBox(10);
+        
+        //poneyBox.setTranslateX(x / 5);
+        //poneyBox.setTranslateY(y / 5);
+        
+       /* Image image = new Image("assets/pony-blue.gif");
         ImageView imageV = new ImageView(image);
         imageV.setFitWidth(75);
         imageV.setFitHeight(75);
@@ -44,7 +75,11 @@ public class SelectPoney extends Parent {
         
         poneyBox.getChildren().addAll(tb3);
         
-        getChildren().addAll(poneyBox);
+        getChildren().addAll(poneyBox);*/
+        
+        System.out.println("EntityBox: " + entityBox[0].getChildren());
+        
+        getChildren().addAll(entityBox[0]);
     }
 }
 
