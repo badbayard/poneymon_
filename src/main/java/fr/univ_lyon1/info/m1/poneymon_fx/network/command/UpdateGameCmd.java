bad6ea1d1510c2c262e4791a6a09a0f8cccd0e6b
Robin Donnay <robin.donnay@etu.univ-lyon1.fr>
2018-11-20 17:22:53 +0100

    Bug correction in createRoomeCmd

---
+++ src/main/java/fr/univ_lyon1/info/m1/poneymon_fx/network/command/UpdateGameCmd.java
@@ -0,0 +1,21 @@
+package fr.univ_lyon1.info.m1.poneymon_fx.network.command;
+
+import fr.univ_lyon1.info.m1.poneymon_fx.model.FieldModel;
+
+public class UpdateGameCmd extends Command {
+    private FieldModel fieldModel;
+
+    public UpdateGameCmd(
+            FieldModel fieldModel) {
+        this.fieldModel = fieldModel;
+    }
+
+    public FieldModel getFieldModel() {
+        return fieldModel;
+    }
+
+    @Override
+    public void atReceive() {
+        System.out.println("Le serveur envois le model");
+    }
+}
